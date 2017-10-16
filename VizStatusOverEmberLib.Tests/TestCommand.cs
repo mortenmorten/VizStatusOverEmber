231c8950e247aa10d496deaebe78a3e4b1ccc07d
Morten Larsen <morten.o.larsen@bouvet.no>
2017-10-16 15:01:04 +0200

    New unit test project with tests for Command parsing

---
+++ VizStatusOverEmberLib.Tests/TestCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace VizStatusOverEmberLib.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestCommand
+    {
+        [Test]
+        public void Constructor_ValidInputStringWithoutId_PropertiesContainsParsedValues()
+        {
+            var cmd = new Command("input 1 active");
+            Assert.That(cmd.Id, Is.EqualTo(-1));
+            Assert.That(cmd.Category, Is.EqualTo("input"));
+            Assert.That(cmd.Name, Is.EqualTo("1"));
+            Assert.That(cmd.Value, Is.EqualTo("active"));
+        }
+
+        [Test]
+        public void Constructor_ValidInputStringWithId_PropertiesContainsParsedValues()
+        {
+            var cmd = new Command("12 input 1 active");
+            Assert.That(cmd.Id, Is.EqualTo(12));
+            Assert.That(cmd.Category, Is.EqualTo("input"));
+            Assert.That(cmd.Name, Is.EqualTo("1"));
+            Assert.That(cmd.Value, Is.EqualTo("active"));
+        }
+
+        [Test]
+        public void Constructor_MissingCategory_ThrowsException()
+        {
+            var cmd = new Command("");
+            Assert.Throws<CommandException>(cmd.ThrowIfDefaultValues);
+        }
+
+        [Test]
+        public void Constructor_MissingName_ThrowsException()
+        {
+            var cmd = new Command("input");
+            Assert.Throws<CommandException>(cmd.ThrowIfDefaultValues);
+        }
+
+        [Test]
+        public void Constructor_MissingValue_ThrowsException()
+        {
+            var cmd = new Command("input 1");
+            Assert.Throws<CommandException>(cmd.ThrowIfDefaultValues);
+        }
+    }
+}
