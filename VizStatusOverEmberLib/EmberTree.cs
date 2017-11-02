7f70b88f5be96faf491fc750bcf80366e8110da0
Morten Larsen <morten.o.larsen@bouvet.no>
2017-11-02 09:52:43 +0100

    Added identity node as per Ember recommendation

--- VizStatusOverEmberLib/EmberTree.cs
+++ VizStatusOverEmberLib/EmberTree.cs
@@ -8,7 +8,9 @@
         public static Node Create(Dispatcher dispatcher)
         {
             var root = Node.CreateRoot();
-            var vizEngine = new Node(1, root, "vizengine");
+            var rootChildIndex = 1;
+            CreateIdentityNode(root, rootChildIndex++, dispatcher);
+            var vizEngine = new Node(rootChildIndex, root, "vizengine");
             CreateInputsParentNode(vizEngine, 1, dispatcher);
             return root;
         }
@@ -16,6 +18,16 @@
         public static bool SetParameter<T>(Dispatcher dispatcher, string path, T value)
         {
             return SetParameter(dispatcher?.Root, path, value);
+        }
+
+        private static void CreateIdentityNode(Element parent, int nodeNumber, Dispatcher dispatcher)
+        {
+            var identityNode = new Node(nodeNumber, parent, "identity");
+            var identityChildIndex = 1;
+            new StringParameter(identityChildIndex++, identityNode, "product", dispatcher, false).Value =
+                "VizStatusOverEmber";
+            new StringParameter(identityChildIndex, identityNode, "version", dispatcher, false).Value =
+                "1.0.0";
         }
 
         private static void CreateInputsParentNode(Element parent, int nodeNumber, Dispatcher dispatcher)
